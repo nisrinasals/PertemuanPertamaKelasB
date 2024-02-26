7dd2f623b7848e283953aab7cc30dd9008e78b1c
nisrinasals <nisrinahikmawan@gmail.com>
2024-02-26 15:19:41 +0700

    Add project files.

---
+++ PertemuanPertamaKelasB/PertemuanPertamaKelasB.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{//begin
+	//numeric nPanjang, nLebar, nLuas
+	//display 'Masukkan Panjang = '
+	//accept nPanjang
+	//display 'Masukkan Lebar = '
+	//acccept nLebar
+	//compute nLuas = nPanjang * nLuas
+	//display 'Luasnya adalah = ' + nLuas
+
+	int nPanjang, nLebar, nLuas;
+	cout << "Masukkan Panjang = ";
+	cin >> nPanjang;
+	cout << "Masukkan Lebar = ";
+	cin >> nLebar;
+	nLuas = nPanjang * nLebar;
+	cout << "Luasnya adalah = " << nLuas << endl;
+}
